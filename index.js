03b2a43314793110c044d693afa4bea773c0e191
daniel_luz <danisluz@gmail.com>
2022-07-21 21:15:10 -0300

    implements async await

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const chalk = require('chalk');
+const fs = require('fs');
 
-console.log(chalk.blue('Hellow world!'));
+function handleError(error){
+  throw new Error(chalk.red(error.code, 'did not find the file'));
+}
 
-const paragrafo = 'Texto retornado dentro de uma função';
-
-function texto(string) {
-  return string
+async function getFile(pathFile){
+    const encoding = 'utf-8';
+    try {
+      const data = await fs.promises.readFile(pathFile, encoding);
+      console.log(chalk.green(data));
+    } catch(error) {
+      handleError(chalk.red(error));
+    }
 };
 
-console.log(chalk.green(texto(paragrafo)));
+// function getFile(pathFile){
+//   const encoding = 'utf-8';
+//   fs.promises
+//     .readFile(pathFile, encoding)
+//     .then((data) => console.log(data))
+//     .catch((error) => handleError(error));
+// }
+
+// function getFile(pathFile){
+//   const encoding = 'utf-8';
+//   fs.readFile(pathFile, encoding, (error, data) => {
+//     if(error){
+//       handleError(error);
+//     }
+//     console.log(chalk.green(data));
+//   });
+// };
+
+getFile('./files/texto1.md');
